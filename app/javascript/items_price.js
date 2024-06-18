eef51902820fa29be9087ee680c21fe4169d97b7
kiki2613 <keywnm@gmail.com>
2024-06-18 18:23:05 +0900

    items_price.jsの編集

--- app/javascript/items_price.js
+++ app/javascript/items_price.js
@@ -1,9 +1,13 @@
-const priceInput = document.getElementById("item-price");
+function price (){
+  window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1);
+    const profit = document.getElementById("profit");
+    profit.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1))
+  })
+})};
 
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1);
-  const profit = document.getElementById("profit");
-  profit.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1))
-})
+window.addEventListener('turbo:load', price);
