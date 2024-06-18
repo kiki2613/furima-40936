37598c01c0c912018015a490eeba704a9e138ce3
kiki2613 <keywnm@gmail.com>
2024-06-18 14:13:19 +0900

    itemsテーブルのカラム名変更
    items_price_id → items_priceへ変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_17_080335) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_18_051130) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -62,10 +62,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_17_080335) do
     t.integer "delivery_price_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "delivery_date_id", null: false
-    t.integer "items_price_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "items_price"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
