5a73767d6ecf3d2d4f2dcbdb3666b15acd0bdd3f
kiki2613 <keywnm@gmail.com>
2024-06-21 12:25:54 +0900

    pay.jpの導入とcard.jsの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,9 @@
 class OrdersController < ApplicationController
-  before_action :item_find, only: :index
 
   def index
+    @item = Item.find(params[:item_id])
     @order = Order.new
     @address = Address.new
   end
 
-  private
-
-  def item_find
-    @item = Item.find(params[:item_id])
-  end
 end
