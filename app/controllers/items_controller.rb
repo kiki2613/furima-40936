2e455674e388d2642ae8b185280b7ae26b1438c4
kiki2613 <keywnm@gmail.com>
2024-06-22 23:29:51 +0900

    販売状況による処理の分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :moved_top_page, only: [:edit, :destroy]
+  before_action :sold_out, only: :edit
 
   def index
     @items = Item.includes(:user).order(created_at: :DESC)
@@ -52,6 +53,12 @@ class ItemsController < ApplicationController
 
   def moved_top_page
     unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
+  def sold_out
+    unless @item.order.present?
       redirect_to root_path
     end
   end
